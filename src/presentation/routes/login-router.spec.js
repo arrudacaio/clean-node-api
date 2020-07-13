73d32b3c4dc23838cf86f4e72a5ad933b3d65dfd
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-07-12 23:54:11 -0300

    ensure LoginRouter returns 500 if an invalid httpRequest is provided

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,6 +1,12 @@
 class LoginRouter {
   route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
     const { email, password } = httpRequest.body
+
     if (!email || !password) {
       return {
         statusCode: 400
@@ -32,5 +38,22 @@ describe('Login Router', () => {
 
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  // Erro no servidor, dev esqueceu do request
+  test('Should return 500 if no httpRequest is provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
